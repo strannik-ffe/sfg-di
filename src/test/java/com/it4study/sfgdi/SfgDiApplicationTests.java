792396d17e56064d8291108cead2af65a4768f2a
ITopalov <topalov.igor@gmail.com>
2021-01-03 18:32:39 -0500

    Initial commmit

---
+++ src/test/java/com/it4study/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it4study.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
